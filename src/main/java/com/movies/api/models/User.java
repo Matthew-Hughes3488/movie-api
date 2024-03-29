5e15a6293e52c30ccc5d1556404e2153d10fdd4a
Matthew <12hughesm@gmail.com>
2024-03-29 14:14:36 +0000

    Created user model

---
+++ src/main/java/com/movies/api/models/User.java
@@ -0,0 +1,64 @@
+package com.movies.api.models;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "registration_date", nullable = false)
+    private Date registrationDate;
+
+    public User(String username, String email, Date registrationDate) {
+        this.username = username;
+        this.email = email;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+}
