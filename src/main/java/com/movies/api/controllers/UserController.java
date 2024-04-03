001b28cae96fcb454806cab3a6ab9b929fad7b9f
Matthew <12hughesm@gmail.com>
2024-04-03 14:38:39 +0100

    created new exception and a UserController.java class

---
+++ src/main/java/com/movies/api/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.movies.api.controllers;
+
+import com.movies.api.models.User;
+import com.movies.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        return userService.getUserById(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.saveUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(id, user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
--- src/main/java/com/movies/api/service/UserService.java
+++ src/main/java/com/movies/api/service/UserService.java
@@ -1,5 +1,6 @@
 package com.movies.api.service;
 
+import com.movies.api.exceptions.NotFoundException;
 import com.movies.api.models.User;
 import com.movies.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,4 +40,24 @@ public class UserService {
     }
 
     // You can define other methods here to interact with the repository as needed
+
+    public User updateUser(Long userId, User updatedUser) {
+        // Retrieve the existing user from the database
+        Optional<User> optionalUser = userRepository.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+
+            // Update the existing user with the new data
+            existingUser.setUsername(updatedUser.getUsername());
+            existingUser.setEmail(updatedUser.getEmail());
+            // Update other fields as needed
+
+            // Save the updated user back to the database
+            return userRepository.save(existingUser);
+        } else {
+            // If the user with the specified ID doesn't exist, you can throw an exception
+            throw new NotFoundException("User not found with id: " + userId);
+        }
+    }
 }
